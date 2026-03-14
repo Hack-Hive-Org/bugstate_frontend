d5e33322906c235c044731c3feca9d438605eded
Sumit240803 <goyalsumit651@gmail.com>
2026-03-14 11:05:59 -0700

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack : {
-    root : "/home/sumit/bugstate/bugstate_frontend/bugstate"
-  }
   /* config options here */
 };
 
